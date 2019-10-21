18844c4d461124c0ddb89fc24516d6b34a3fa9a3
mafh-company <necrodriver@foxmail.com>
2019-10-21 22:03:28 +0800

    配置文件仓库文件提交

--- service-config/src/main/java/com/xin/config/ServiceConfigApplication.java
+++ service-config/src/main/java/com/xin/config/ServiceConfigApplication.java
@@ -2,8 +2,15 @@ package com.xin.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * @author mafh
+ */
 @SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
 public class ServiceConfigApplication {
 
     public static void main(String[] args) {
