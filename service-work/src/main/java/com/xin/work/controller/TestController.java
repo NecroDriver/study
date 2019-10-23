72f13f6a7eca09490c8741e2edf47ebed86a6c91
mafh-company <necrodriver@foxmail.com>
2019-10-23 18:10:13 +0800

    '配置中心'

--- service-work/src/main/java/com/xin/work/controller/TestController.java
+++ service-work/src/main/java/com/xin/work/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.xin.work.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +10,22 @@ import org.springframework.web.bind.annotation.RestController;
  * @author creator mafh 2019/10/21 14:20
  * @author updater
  * @version 1.0.0
+ * @RefreshScope 使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中。
  */
 @RestController
+@RefreshScope
 public class TestController {
+
+    @Value("${test-name}")
+    private String testName;
 
     @RequestMapping("/hello")
     public String getIndex(@RequestParam String name) {
         return name + ",this is a test controller";
+    }
+
+    @RequestMapping("/getConfig")
+    public String getConfig() {
+        return testName;
     }
 }
