36388b20d61c81a5004e59f43d80ff05ca378509
mafh-company <necrodriver@foxmail.com>
2019-10-31 10:31:51 +0800

    'jdbc项目创建'

---
+++ boot-starter-jdbc/src/main/java/com/xin/jdbc/BootStarterJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.xin.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootStarterJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootStarterJdbcApplication.class, args);
+    }
+
+}
