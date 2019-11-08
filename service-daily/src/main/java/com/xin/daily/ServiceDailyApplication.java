d68560c2e8e9d8e2818334c0f8ba287dc8b135c2
mafh-company <necrodriver@foxmail.com>
2019-11-08 19:07:53 +0800

    '引用修改'

--- service-daily/src/main/java/com/xin/daily/ServiceDailyApplication.java
+++ service-daily/src/main/java/com/xin/daily/ServiceDailyApplication.java
@@ -1,10 +1,12 @@
 package com.xin.daily;
 
+import com.xin.swagger.configuration.EnableStudySwagger2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = "com.xin")
+@EnableStudySwagger2
 public class ServiceDailyApplication {
 
     public static void main(String[] args) {
