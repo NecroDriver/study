bf949c101f0322926f56db530e4591b6e5eb0f6d
NecroDriver <necroDriver@foxmail.com>
2019-11-10 23:45:35 +0800

    '自启失败，项目重新部署结构异常'

--- swagger-spring-boot-starter/src/main/java/com/xin/swagger/SwaggerSpringBootStarterApplication.java
+++ swagger-spring-boot-starter/src/main/java/com/xin/swagger/SwaggerSpringBootStarterApplication.java
@@ -1,9 +1,11 @@
 package com.xin.swagger;
 
+import com.xin.swagger.configuration.EnableStudySwagger2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableStudySwagger2
 public class SwaggerSpringBootStarterApplication {
 
     public static void main(String[] args) {
