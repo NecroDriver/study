1dd893f98bd0f06a0871c022b2d46e2113b47114
mafh-company <necrodriver@foxmail.com>
2019-11-12 16:04:23 +0800

    '添加链接属性'

--- druid-spring-boot-starter/src/main/java/com/xin/druid/configuration/DruidProperties.java
+++ druid-spring-boot-starter/src/main/java/com/xin/druid/configuration/DruidProperties.java
@@ -12,6 +12,10 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "spring.datasource.druid")
 public class DruidProperties {
 
+    private String name;
+    private String url;
+    private String username;
+    private String password;
     /**
      * 初始化大小
      */
@@ -121,6 +125,38 @@ public class DruidProperties {
         public void setLoginPassword(String loginPassword) {
             this.loginPassword = loginPassword;
         }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public Integer getInitialSize() {
