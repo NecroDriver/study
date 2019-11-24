43bd33575782db616ac28e5c943acf04eae47b35
NecroDriver <necroDriver@foxmail.com>
2019-11-24 23:27:11 +0800

    'redis工具包提交'

--- jar/redis-spring-boot-starter/src/main/java/com/xin/redis/configuration/RedisAutoConfiguration.java
+++ jar/redis-spring-boot-starter/src/main/java/com/xin/redis/configuration/RedisAutoConfiguration.java
@@ -1,26 +1,21 @@
 package com.xin.redis.configuration;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
  * redis配置类
+ * @ EnableCaching 允许缓存
  *
  * @author creator mafh 2019/11/22 18:04
  * @author updater
  * @version 1.0.0
- * @ EnableCaching 允许缓存
  */
 @Configuration
 @EnableCaching
@@ -34,20 +29,11 @@ public class RedisAutoConfiguration extends CachingConfigurerSupport {
      */
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
-
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         // 配置连接工厂
         redisTemplate.setConnectionFactory(factory);
-        //使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值（默认使用JDK的序列化方式）
-        Jackson2JsonRedisSerializer jacksonSerial = new Jackson2JsonRedisSerializer(Object.class);
-
-        ObjectMapper om = new ObjectMapper();
-        // 指定要序列化的域，field,get和set,以及修饰符范围，ANY是都有包括private和public
-        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        // 指定序列化输入的类型，类必须是非final修饰的，final修饰的类，比如String,Integer等会抛出异常
-        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-        jacksonSerial.setObjectMapper(om);
-
+        //使用GenericJackson2JsonRedisSerializer来序列化和反序列化redis的value值（默认使用JDK的序列化方式）
+        GenericJackson2JsonRedisSerializer jacksonSerial = new GenericJackson2JsonRedisSerializer();
         // 值采用json序列化
         redisTemplate.setValueSerializer(jacksonSerial);
         //使用StringRedisSerializer来序列化和反序列化redis的key值
@@ -56,11 +42,6 @@ public class RedisAutoConfiguration extends CachingConfigurerSupport {
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
         redisTemplate.setHashValueSerializer(jacksonSerial);
         redisTemplate.afterPropertiesSet();
-
         return redisTemplate;
-    }
-
-    public ValueOperations<String, Object> valueOperations(){
-        return redisTemplate().opsForValue();
     }
 }
