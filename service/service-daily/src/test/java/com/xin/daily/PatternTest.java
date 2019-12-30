e3c2682af452f88418f17afa3a0e72e81a2d41b4
mafh-company <necrodriver@foxmail.com>
2019-12-30 18:31:33 +0800

    '限制请求'

--- service/service-daily/src/test/java/com/xin/daily/PatternTest.java
+++ service/service-daily/src/test/java/com/xin/daily/PatternTest.java
@@ -1,9 +1,19 @@
 package com.xin.daily;
 
+import com.xin.web.utils.convert.JsonUtils;
+
+import java.util.regex.Pattern;
+
 /**
  * @author creator mafh 2019/12/27 13:18
  * @author updater
  * @version 1.0.0
  */
 public class PatternTest {
+
+    public static void main(String[] args) {
+        String url = "/branchManage/res/toMediaCheckList.html";
+        String[] arr1= url.split("/");
+        System.out.println(JsonUtils.toJson(arr1));
+    }
 }
